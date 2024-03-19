fe745a19935de1393d0e23fd90fac90d8e94ca7f
silviaBeshay <sbesha01@gmail.com>
2024-03-19 18:26:28 -0400

    first commit

---
+++ src/main/java/org/example/T1.java
@@ -0,0 +1,26 @@
+package org.example;
+
+public class T1 {
+
+
+    /*.	Create a program that uses an array to store a list of temperatures for
+     a week, and then uses a loop to find the highest and lowest temperature for the week.*/
+        public static void main(String[] args) {
+            int[] temp={-1,25,-5,10,16,22,8};
+            int high=temp[0];
+            int low=temp[0];
+            for (int n:temp){
+                if (n>high){
+                    high=n;
+                }
+                if (n<low){
+                    low=n;
+                }
+            }
+            System.out.println("the Highest Temperature during the week is "+high);
+            System.out.println("the Lowest Temperature during the week is "+low);
+
+        }
+    }
+
+
---
+++ src/main/java/org/example/T2.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class T2 {
+    public static void main(String[] args) {
+        int[] num={20,40,60,10,80};
+        int sum=0;
+        for (int n:num){
+            sum+=n;
+        }
+        System.out.println("the Sum of all Elements is "+sum);
+
+
+    }
+}
